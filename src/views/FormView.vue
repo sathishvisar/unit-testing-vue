aba9e3ba548eae479ee1252c1de665dd8d7967c6
Sathishkumar <sathish.visar@gmail.com>
2024-07-03 15:10:39 +0530

    set login bg

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -64,5 +64,12 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background-color: #24263a;
+  background-image: url(https://app.campaygn.com/img/login_bg.44c3fe35.png);
+  background-size: cover;
+  min-height: 100vh;
 }
 </style>
